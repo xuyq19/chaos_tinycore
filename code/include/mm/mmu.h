8e00edb2a6612873b1bedade4bb0ccb605fff9ad
xuyq19 <xuyuqiabcd@outlook.com>
2023-04-15 20:47:46 +0800

    init

---
+++ code/include/mm/mmu.h
@@ -0,0 +1,7 @@
+#ifndef __CODE_MM_MMU_H__
+#define __CODE_MM_MMU_H__
+
+#define PAGE_SIZE 4096 // 4KB Page Size
+#define PAGE_SHIFT 12  // 2^12 = 4096
+
+#endif
---
+++ code/include/sbi.h
@@ -0,0 +1,17 @@
+#ifndef __CODE_INCLUDE_SBI_H__
+#define __CODE_INCLUDE_SBI_H__
+
+/** error code */
+#define SBI_SUCCESS 0
+#define SBI_ERR_FAILED -1
+#define SBI_ERR_NOT_SUPPORTED -2
+#define SBI_ERR_INVALID_PARAM -3
+#define SBI_ERR_DENIED -4
+#define SBI_ERR_INVALID_ADDRESS -5
+#define SBI_ERR_ALREADY_AVAILABLE -6
+#define SBI_ERR_ALREADY_STARTED -7
+#define SBI_ERR_ALREADY_STOPPED -8
+
+/** extension id */
+
+#define SBI_EXT_SET_TIMER 0x00
