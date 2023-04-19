550d5ed61383dbe1828a74f7d352569ae36b4db4
xuyq19 <xuyuqiabcd@outlook.com>
2023-04-19 16:08:54 +0800

    finish linked list head file

--- code/include/wheels/linked_list.h
+++ code/include/wheels/linked_list.h
@@ -55,7 +55,7 @@ static inline void insert_node_after(struct linked_list_node *new_node,
  *
  * @param target 要删除的节点，不能为NULL
  */
-* / static inline void delete_node(struct linked_list_node *target) {
+static inline void delete_node(struct linked_list_node *target) {
   target->next->prev = target->prev;
   target->prev->next = target->next;
 }
@@ -90,3 +90,58 @@ static inline struct linked_list_node *
 get_last_node(struct linked_list_node *list) {
   return list->prev;
 }
+
+/**
+ * @brief Insert the node to the last of the list
+ *
+ * @param new_node
+ * @param list
+ */
+static inline void insert_node_to_last(struct linked_list_node *new_node,
+                                       struct linked_list_node *list) {
+  insert_node_before(new_node, list);
+}
+
+/**
+ * @brief Return the last node and delete it
+
+ *
+ */
+static inline struct linked_list_node *
+linked_list_pop(struct linked_list_node *list) {
+  struct linked_list_node *last = get_last_node(list);
+  delete_node(last);
+  return last;
+}
+
+/**
+ * @brief Insert the node to the first of the list
+ *
+ * @param new_node
+ * @param list
+ */
+static inline void insert_node_to_first(struct linked_list_node *new_node,
+                                        struct linked_list_node *list) {
+  insert_node_after(new_node, list);
+}
+
+/**
+ * @brief Return the first node and delete it
+ *
+ * @param list
+ * @return struct linked_list_node*
+ */
+static inline struct linked_list_node *
+linked_list_shift(struct linked_list_node *list) {
+  struct linked_list_node *first = get_first_node(list);
+  delete_node(first);
+  return first;
+}
+
+/**
+ * @brief Get the node by index
+ *
+ */
+#define for_each_node(node, list)                                              \
+  for (node = get_first_node(list); node != list; node = node->next)
+#endif
